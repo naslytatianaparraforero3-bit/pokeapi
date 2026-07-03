382735902beced15d230109ae56742b4e61f65df
devstiven12 <devstiven12@gmail.com>
2026-07-02 21:29:02 -0500

    feat: se crea los components

---
+++ poke/src/app/features/pokemon/components/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Pokemon } from '../../model/pokemon.model';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.scss']
+})
+export class PokemonCardComponent {
+  @Input() pokemon!: Pokemon;
+
+  getMainType(): string {
+    return this.pokemon.types[0]?.type.name || 'normal';
+  }
+
+  getStatValue(statName: string): number {
+    const stat = this.pokemon.stats.find(s => s.stat.name === statName);
+    return stat ? stat.base_stat : 0;
+  }
+}
