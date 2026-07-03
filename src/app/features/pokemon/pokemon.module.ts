1955c22c416dcea1bf6711020b730b0e0b7b117e
devstiven12 <devstiven12@gmail.com>
2026-07-02 22:35:49 -0500

    se soluciona error satndalone false

---
+++ src/app/features/pokemon/pokemon.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component'; // more
+
+@NgModule({
+  declarations: [
+    PokemonListComponent,
+    PokemonCardComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PokemonListComponent
+  ]
+})
+export class PokemonModule { }
